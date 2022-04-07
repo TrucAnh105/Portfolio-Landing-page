9745d7ae428d5f92e84028c551683ec1174766df
truc <tr@g.com>
2022-04-07 20:59:19 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    //typing animation
+ //typing animation
     var typed = new Typed(".typing", {
         strings:["Photographer","UI/UX Design","Web Development","Graphic Design"],
         typeSpeed: 100,
@@ -7,6 +7,20 @@ $(document).ready(function(){
         loop: true
     });
 });
+ //moving button
+const filter_btns = document.querySelectorAll(".filter-btn");
+filter_btns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+      console.log('click here');
+    filter_btns.forEach((button) => button.classList.remove("active"));
+    btn.classList.add("active");
+
+    let filterValue = btn.dataset.filter;
+
+    $(".grid").isotope({ filter: filterValue });
+  })
+);
+//transition grid
 $('.grid').isotope({
     itemSelector: '.grid-item',
     layoutMode: 'fitRows',
