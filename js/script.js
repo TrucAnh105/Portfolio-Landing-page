c2ebb2d650dab9a4831bac3cda4d0ad3db2939a9
truc <tr@g.com>
2022-04-08 08:17:26 +0700

    fix mobile

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
+const skills_wrap = document.querySelector(".skills");
+const skills_bars = document.querySelectorAll(".skill-progress");
+
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
+}
+
+hamburger_menu.addEventListener("click", () => {
+  if (!navbar.classList.contains("open")) {
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scrolling");
+  } else {
+    closeMenu();
+  }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
+
+
+window.addEventListener("scroll", () => {
+  skillsEffect();
+});
+
+function checkScroll(el) {
+  let rect = el.getBoundingClientRect();
+  if (window.innerHeight >= rect.top + el.offsetHeight) return true;
+  return false;
+}
+
+function skillsEffect() {
+  if (!checkScroll(skills_wrap)) return;
+  skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
+}
+
+ 
+
 $(document).ready(function(){
  //typing animation
     var typed = new Typed(".typing", {
